871b08fed33b14b5f629c5bdf85aca9d339e3c4b
jrgatsb <jrg@statsbiblioteket.dk>
2013-09-24 16:52:44 +0200

    minor update to CreateBatch

--- batch-trigger/src/main/java/dk/statsbiblioteket/newspaper/batcheventFramework/CreateBatch.java
+++ batch-trigger/src/main/java/dk/statsbiblioteket/newspaper/batcheventFramework/CreateBatch.java
@@ -9,17 +9,35 @@ package dk.statsbiblioteket.newspaper.batcheventFramework;
  */
 public class CreateBatch {
     public static void main(String[] args) {
-        System.out.println("CreateBatch says:");
-        if (args.length != 6) {
-			System.out.println("Not the right amount of arguments");
-		}
+        String domsUrl;
+        String domsUser;
+        String domsPass;
+        String premisAgent;
+        String batchId;
+        String roundTrip;
 
+        if (args.length != 6) {
+            System.out.println("Not the right amount of arguments");
+        }
+
+        // For testing purposes, remove
+        System.out.println("CreateBatch says:");
         System.out.println("Received:" + args[0]);
         System.out.println("Received:" + args[1]);
         System.out.println("Received:" + args[2]);
         System.out.println("Received:" + args[3]);
         System.out.println("Received:" + args[4]);
         System.out.println("Received:" + args[5]);
+
+        batchId = args[0];
+        roundTrip = args[1];
+        premisAgent = args[2];
+        domsUrl = args[3];
+        domsUser = args[4];
+        domsPass = args[0];
+
+        // TODO
+
     }
 }
 
--- batch-trigger/src/main/scripts/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(dirname $(readlink -f $0))
-
-java -classpath $SCRIPT_DIR/../lib/'*' dk.statsbiblioteket.newspaper.batcheventFramework.CreateBatch
