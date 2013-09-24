da076c4dcc70f45ef473b9cbf9ee2005f6067cb6
jrgatsb <jrg@statsbiblioteket.dk>
2013-09-24 14:00:29 +0200

    trigger now does not re-process batches that have already been processed

--- batch-trigger/trigger-on-new-batch.sh
+++ batch-trigger/trigger-on-new-batch.sh
@@ -3,27 +3,48 @@
 # Check for newly arrived newspaper batch(es), and initiate creation in DOMS
 #
 
-CONFIG_FILENAME="$1"
-if [ -z "$CONFIG_FILENAME" ]; then
+donedir='batches-done'
+
+config_filename="$1"
+if [ -z "$config_filename" ]; then
 	echo 'config-filename not received' >&2
 	exit 1
 fi
 
 # Get config from file
-source "$CONFIG_FILENAME"
+source "$config_filename"
 
-cd "$PATH_TO_DIR_OF_BATCHES"
+# Get path to directory of this script
+my_path="`dirname \"$0\"`"              # relative
+my_path="`( cd \"$my_path\" && pwd )`"  # absolutized and normalized
+if [ -z "$my_path" ] ; then
+	echo 'could not access path' >&2
+    exit 1
+fi
+
+cd "$path_to_dir_of_batches"
 
 for batch_dirname in *; do
 	# Check format of dirname
 	if [[ ! "$batch_dirname" =~ ^B[^-]+\-RT[0-9]+$ ]]; then
-	#if [[ "$batch_dirname" =~ "^B[^-]+\-RT[0-9]+$" ]]; then
 		# Dirname not recognized as a batch, skip it
 		continue
 	fi
-	echo "'$batch_dirname'"
+
+	# Skip batches that are already done
+	if [ -f "$my_path/$donedir/$batch_dirname" ]; then
+		continue
+	fi
 
 	batch_id=`echo "$batch_dirname" | sed -r 's/^B([^-]+).+/\1/g'`
 	roundtrip=`echo "$batch_dirname" | sed -r 's/^B[^-]+\-RT([0-9]+)/\1/g'`
+
+	# Create batch in DOMS
+	# TODO
+	echo $batch_id
+	echo $roundtrip
+
+	# Mark batch as done, by creating an empty file with the batch's name
+	touch "$my_path/$donedir/$batch_dirname"
 done
 
