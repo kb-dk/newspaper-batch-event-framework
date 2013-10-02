197d2608c38a7864463730fce552b02495e3c615
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-10-02 17:20:08 +0200

    Added a little javadoc

--- batch-event-client/src/main/java/dk/statsbibliokeket/newspaper/batcheventFramework/BatchEventClient.java
+++ batch-event-client/src/main/java/dk/statsbibliokeket/newspaper/batcheventFramework/BatchEventClient.java
@@ -3,6 +3,9 @@ package dk.statsbibliokeket.newspaper.batcheventFramework;
 import dk.statsbiblioteket.newspaper.batcheventFramework.DomsEventClient;
 import dk.statsbiblioteket.newspaper.processmonitor.datasources.SBOIInterface;
 
+/**
+ * The interface for the batch event client. It is just a combination of the DomsEventClient and the SBOIInterface
+ */
 public interface BatchEventClient extends DomsEventClient,SBOIInterface{
 
 }
