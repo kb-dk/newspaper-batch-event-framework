1e80d635246bae6a35231ccc7f1f3c22723b0b7c
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-10-08 18:11:24 +0200

    SBOI tests out again

--- sboi-datasource/src/test/java/dk/statsbiblioteket/newspaper/SBOIDatasourceTest.java
+++ sboi-datasource/src/test/java/dk/statsbiblioteket/newspaper/SBOIDatasourceTest.java
@@ -1,42 +1,77 @@
 package dk.statsbiblioteket.newspaper;
 
+import dk.statsbibliokeket.newspaper.batcheventFramework.BatchEventClientImpl;
+import dk.statsbiblioteket.newspaper.processmonitor.datasources.Batch;
+import dk.statsbiblioteket.newspaper.processmonitor.datasources.CommunicationException;
 import dk.statsbiblioteket.newspaper.processmonitor.datasources.DataSource;
 import dk.statsbiblioteket.newspaper.processmonitor.datasources.EventID;
+import dk.statsbiblioteket.newspaper.processmonitor.datasources.NotFoundException;
 import dk.statsbiblioteket.newspaper.processmonitor.datasources.TCKTestSuite;
 import dk.statsbiblioteket.util.Pair;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.Properties;
 
-public class SBOIDatasourceTest extends TCKTestSuite {
+public class SBOIDatasourceTest
+        extends TCKTestSuite {
+
+    private static final long MINUTES = 60 * 1000;
+    private DataSource dataSource;
 
     @Override
-    public DataSource getDataSource() {
-        Properties props = new Properties();
+    public synchronized DataSource getDataSource() {
+        if (dataSource == null) {
+            Properties props = new Properties();
+            try {
+                props.load(Thread.currentThread()
+                                 .getContextClassLoader()
+                                 .getResourceAsStream("ITtest.properties"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            SBOIDatasourceConfiguration conf = new SBOIDatasourceConfiguration();
 
-        try {
-            props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("ITtest.properties"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        SBOIDatasourceConfiguration conf = new SBOIDatasourceConfiguration();
+            conf.setDomsUrl(props.getProperty("fedora.location"));
+            conf.setDomsUser(props.getProperty("fedora.username"));
+            conf.setDomsPass(props.getProperty("fedora.password"));
+            conf.setUrlToPidGen(props.getProperty("pidgenerator.location"));
+            conf.setSummaLocation(props.getProperty("sboi.summa"));
+            dataSource = new SBOIDatasource(conf);
+            BatchEventClientImpl batchClient =
+                    new BatchEventClientImpl(conf.getSummaLocation(), conf.getDomsUrl(), conf.getDomsUser(),
+                                             conf.getDomsPass(), conf.getUrlToPidGen());
+            try {
+                try {
+                    Batch testBatch = batchClient.getBatch(getValidBatchID().getLeft(), getValidBatchID().getRight());
+                } catch (NotFoundException e) {
+                    //So, the test batch was not in the system
 
-        conf.setDomsUrl(props.getProperty("fedora.location"));
-        conf.setDomsUser(props.getProperty("fedora.username"));
-        conf.setDomsPass(props.getProperty("fedora.password"));
-        conf.setUrlToPidGen(props.getProperty("pidgenerator.location"));
-        conf.setSummaLocation(props.getProperty("sboi.summa"));
-        return new SBOIDatasource(conf);
+                    //Add it
+                    batchClient.addEventToBatch(getValidBatchID().getLeft(), getValidBatchID().getRight(), "SBOI unit test",new Date(),"no details",getValidAndSucessfullEventIDForValidBatch(),true);
+
+                    try {
+                        //Wait for the Summa system to reindex the doms
+                        Thread.sleep(6*MINUTES);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                    }
+                }
+
+            } catch (CommunicationException e) {
+                throw new RuntimeException(e);
+            }
+        } return dataSource;
     }
 
     @Override
-    public Pair<Long,Integer> getValidBatchID() {
-        return new Pair<>(400022028241l,2);
+    public Pair<Long, Integer> getValidBatchID() {
+        return new Pair<>(400022028241l, 2);
     }
 
     @Override
-    public Pair<Long,Integer> getInvalidBatchID() {
-        return new Pair<>(300022028241l,1);
+    public Pair<Long, Integer> getInvalidBatchID() {
+        return new Pair<>(300022028241l, 1);
     }
 
     @Override
