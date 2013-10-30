a263eec674b7b97cd0427a79725839f2be44be52
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-10-30 11:25:28 +0100

    Iterator for fedora 3 now have a working integrationtest

--- tree-processor/src/test/java/dk/statsbiblioteket/medieplatform/autonomous/iterator/fedora3/IteratorForFedora3Test.java
+++ tree-processor/src/test/java/dk/statsbiblioteket/medieplatform/autonomous/iterator/fedora3/IteratorForFedora3Test.java
@@ -2,10 +2,16 @@ package dk.statsbiblioteket.medieplatform.autonomous.iterator.fedora3;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import dk.statsbiblioteket.doms.central.connectors.BackendInvalidCredsException;
+import dk.statsbiblioteket.doms.central.connectors.BackendMethodFailedException;
+import dk.statsbiblioteket.doms.central.connectors.EnhancedFedoraImpl;
+import dk.statsbiblioteket.doms.central.connectors.fedora.pidGenerator.PIDGeneratorException;
+import dk.statsbiblioteket.doms.webservices.authentication.Credentials;
 import dk.statsbiblioteket.medieplatform.autonomous.AbstractTests;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.TreeIterator;
 import org.testng.annotations.Test;
 
+import javax.xml.bind.JAXBException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -21,18 +27,32 @@ public class IteratorForFedora3Test extends AbstractTests {
 
     @Override
     public TreeIterator getIterator() throws URISyntaxException, IOException {
-        if (iterator == null){
+        if (iterator == null) {
+
+
             Properties properties = new Properties();
-            properties.load(new FileReader(new File(System.getProperty(
-                    "integration.test.newspaper.properties"))));
+            properties.load(new FileReader(new File(System.getProperty("integration.test.newspaper.properties"))));
             System.out.println(properties.getProperty("fedora.admin.username"));
             Client client = Client.create();
-            client.addFilter(new HTTPBasicAuthFilter(properties.getProperty("fedora.admin.username"),
-                    properties.getProperty("fedora.admin.password")));
+            client.addFilter(new HTTPBasicAuthFilter(properties.getProperty("fedora.admin.username"), properties
+                    .getProperty("fedora.admin.password")));
+
+            String pid;
+            try {
+                EnhancedFedoraImpl fedora = new EnhancedFedoraImpl(new Credentials(properties.getProperty("fedora.admin.username"),
+                                                                                   properties.getProperty("fedora.admin.password")),
+                                                                   properties.getProperty("fedora.server")
+                                                                             .replaceFirst("/(objects)?/?$", ""),
+                                                                   null,
+                                                                   null);
+                pid = fedora.findObjectFromDCIdentifier("path:B400022028246-RT1").get(0);
+            } catch (PIDGeneratorException | BackendMethodFailedException | JAXBException | BackendInvalidCredsException e) {
+                throw new RuntimeException(e);
+            }
 
             // The uuid below is for a test object ingested by someone and may never be deleted?!
-            iterator = new IteratorForFedora3("uuid:0567a139-d493-4924-b1a9-271a2ea9c044", client,
-                    properties.getProperty("fedora.server"), new TestFilter());
+
+            iterator = new IteratorForFedora3(pid, client, properties.getProperty("fedora.server"), new TestFilter());
         }
         return iterator;
     }
@@ -52,12 +72,12 @@ public class IteratorForFedora3Test extends AbstractTests {
         }
     }
 
-    @Test(groups = "integrationTest", enabled = false)
+    @Test(groups = "integrationTest", enabled = true)
     public void testIterator() throws Exception {
         super.testIterator(true,false);
     }
 
-    @Test(groups = "integrationTest", enabled = false)
+    @Test(groups = "integrationTest", enabled = true)
     public void testIteratorWithSkipping() throws Exception {
         super.testIteratorWithSkipping(false,false);
     }
