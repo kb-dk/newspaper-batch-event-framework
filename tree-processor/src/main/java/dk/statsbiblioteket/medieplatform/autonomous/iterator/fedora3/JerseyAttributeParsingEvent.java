f94b16cbd879cf9cf0f2984a277192a368e5854c
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2013-10-30 18:53:29 +0100

    Review followup: Cache checksum and better error message

--- tree-processor/src/main/java/dk/statsbiblioteket/medieplatform/autonomous/iterator/fedora3/JerseyAttributeParsingEvent.java
+++ tree-processor/src/main/java/dk/statsbiblioteket/medieplatform/autonomous/iterator/fedora3/JerseyAttributeParsingEvent.java
@@ -14,11 +14,12 @@ import java.util.regex.Pattern;
  */
 public class JerseyAttributeParsingEvent extends AttributeParsingEvent {
     private static final Pattern CHECKSUM_PATTERN = Pattern.compile("<dsChecksum>([^>]*)</dsChecksum>");
+    /** The webresource that represents this datastream */
+    private final WebResource resource;
+    /** Contains the checksum. Retrieved and stored on first request */
+    private String checksum;
 
-    private WebResource resource;
-
-    public JerseyAttributeParsingEvent(String name,
-                                       WebResource resource) {
+    public JerseyAttributeParsingEvent(String name, WebResource resource) {
         super(name);
         this.resource = resource;
     }
@@ -29,17 +30,18 @@ public class JerseyAttributeParsingEvent extends AttributeParsingEvent {
         if (response.getStatus() >= 200 && response.getStatus() < 300){
             return response.getEntityInputStream();
         }
-        throw new IOException(response.getStatus()+"");
+        throw new IOException("Unable to get content: " + response.getStatus());
     }
 
     @Override
     public String getChecksum() throws IOException {
-        String response = resource.queryParam("format", "XML").get(String.class);
-        Matcher matcher = CHECKSUM_PATTERN.matcher(response);
-        if (matcher.find()) {
-            return matcher.group(1);
-        } else {
-            return null;
+        if (checksum == null) {
+            String response = resource.queryParam("format", "XML").get(String.class);
+            Matcher matcher = CHECKSUM_PATTERN.matcher(response);
+            if (matcher.find()) {
+                checksum = matcher.group(1);
+            }
         }
+        return checksum;
     }
 }
