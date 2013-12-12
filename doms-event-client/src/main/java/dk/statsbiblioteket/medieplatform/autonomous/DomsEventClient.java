2129242c0cf36e781db00f58eacd20dda8e24859
Colin Rosenthal <csr@statsbiblioteket.dk>
2013-12-12 11:28:50 +0100

    Follow-ups to https://sbforge.org/fisheye/cru/CR-NEWSPAPER-111.

--- doms-event-client/src/main/java/dk/statsbiblioteket/medieplatform/autonomous/DomsEventClient.java
+++ doms-event-client/src/main/java/dk/statsbiblioteket/medieplatform/autonomous/DomsEventClient.java
@@ -56,8 +56,33 @@ public interface DomsEventClient {
     /**
      * This method
      * i) reads the EVENTS datastream
-     * ii) Calculates the number of events to be removed from the EVENTS datastream all events starting with the
-     * earliest failure or the given eventId
+     * ii) Calculates the number of events to be removed from the EVENTS datastream starting with the
+     *  given eventId
+     * iii) if there are no events to be removed it just returns zero, otherwise it
+     * iv) makes a backup of the EVENTS datastream for this batch round trip
+     * v) writes the modfied EVENTS datastream back to DOMS
+     *
+     * It is the job of implementers of this method to ensure that it correctly handles the possibility of concurrent
+     * modification ie. that the datastream may have changed again between being read and being written. (In which case
+     * one should return to step i).
+     *
+     *
+     *
+     * @param batchId
+     * @param roundTripNumber
+     * @param maxTries the maximum number of attempts.
+     * @param waitTime the time in milliseconds to wait between attempts.
+     * @param eventId The eventId of the of the earliest event to be removed
+     * @return the number of events removed.
+     * @throws CommunicationException
+     */
+    int triggerWorkflowRestartFromFirstFailure(String batchId, int roundTripNumber, int maxTries, long waitTime, String eventId) throws CommunicationException, NotFoundException;
+
+    /**
+     * This method
+     * i) reads the EVENTS datastream
+     * ii) Calculates the number of events to be removed from the EVENTS datastream starting with the
+     * earliest failure
      * iii) if there are no events to be removed it just returns zero, otherwise it
      * iv) makes a backup of the EVENTS datastream for this batch round trip
      * v) writes the modfied EVENTS datastream back to DOMS
@@ -71,11 +96,11 @@ public interface DomsEventClient {
      * @param roundTripNumber
      * @param maxTries the maximum number of attempts.
      * @param waitTime the time in milliseconds to wait between attempts.
-     * @param eventId The eventId of the of the earliest event to be removed, or null if all events after the first
-     *                failure are to be removed.
+
      * @return the number of events removed.
      * @throws CommunicationException
      */
-    int triggerWorkflowRestartFromFirstFailure(String batchId, int roundTripNumber, int maxTries, long waitTime, String eventId) throws CommunicationException;
+    int triggerWorkflowRestartFromFirstFailure(String batchId, int roundTripNumber, int maxTries, long waitTime) throws CommunicationException, NotFoundException;
+
 
 }
