52d64225bb0c535ac596a8d52ee2640b629b934f
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-12-17 13:18:40 +0100

    Initilize the summa webservice once, not for each call

--- batch-event-client/src/main/java/dk/statsbibliokeket/newspaper/batcheventFramework/SBOIClientImpl.java
+++ batch-event-client/src/main/java/dk/statsbibliokeket/newspaper/batcheventFramework/SBOIClientImpl.java
@@ -2,10 +2,10 @@ package dk.statsbibliokeket.newspaper.batcheventFramework;
 
 import dk.statsbiblioteket.doms.central.summasearch.SearchWS;
 import dk.statsbiblioteket.doms.central.summasearch.SearchWSService;
-import dk.statsbiblioteket.medieplatform.autonomous.PremisManipulatorFactory;
 import dk.statsbiblioteket.medieplatform.autonomous.Batch;
 import dk.statsbiblioteket.medieplatform.autonomous.CommunicationException;
 import dk.statsbiblioteket.medieplatform.autonomous.NotFoundException;
+import dk.statsbiblioteket.medieplatform.autonomous.PremisManipulatorFactory;
 import dk.statsbiblioteket.util.xml.DOM;
 import dk.statsbiblioteket.util.xml.XPathSelector;
 import net.sf.json.JSONObject;
@@ -16,15 +16,13 @@ import org.w3c.dom.NodeList;
 
 import javax.xml.namespace.QName;
 import java.io.ByteArrayInputStream;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-/**
- * Implementation of the SBOI interface. Uses soap, json and xml to query the summa instance for batches
- */
-public class SBOIClientImpl
-        implements SBOIInterface {
+/** Implementation of the SBOI interface. Uses soap, json and xml to query the summa instance for batches */
+public class SBOIClientImpl implements SBOIInterface {
 
     private static final String SUCCESSEVENT = "success_event";
     private static final String FAILEVENT = "fail_event";
@@ -32,46 +30,37 @@ public class SBOIClientImpl
     private static final String ROUND_TRIP_NO = "round_trip_no";
     private static final String BATCH_ID = "batch_id";
     private static final String PREMIS = "premis";
-
-    private static Logger log = org.slf4j
-            .LoggerFactory
-            .getLogger(BatchEventClientImpl.class);
-    private String summaLocation;
+    private static Logger log = org.slf4j.LoggerFactory.getLogger(BatchEventClientImpl.class);
     private final PremisManipulatorFactory premisManipulatorFactory;
+    private SearchWS summaSearch;
 
-    public SBOIClientImpl(String summaLocation, PremisManipulatorFactory premisManipulatorFactory1) {
-        this.summaLocation = summaLocation;
-        this.premisManipulatorFactory = premisManipulatorFactory1;
+    public SBOIClientImpl(String summaLocation, PremisManipulatorFactory premisManipulatorFactory) throws
+                                                                                                   MalformedURLException {
+        this.premisManipulatorFactory = premisManipulatorFactory;
+        summaSearch = new SearchWSService(
+                new java.net.URL(summaLocation),
+                new QName("http://statsbiblioteket.dk/summa/search", "SearchWSService")).getSearchWS();
+
     }
 
     @Override
-    public Iterator<Batch> getBatches(List<String> pastSuccessfulEvents,
-                                      List<String> pastFailedEvents,
-                                      List<String> futureEvents)
-            throws
-            CommunicationException {
+    public Iterator<Batch> getBatches(List<String> pastSuccessfulEvents, List<String> pastFailedEvents,
+                                      List<String> futureEvents) throws CommunicationException {
 
         return search(null, null, pastSuccessfulEvents, pastFailedEvents, futureEvents);
     }
 
-
     @Override
-    public Iterator<Batch> search(String batchID,
-                                   Integer roundTripNumber,
-                                   List<String> pastSuccessfulEvents,
-                                   List<String> pastFailedEvents,
-                                   List<String> futureEvents)
-            throws
-            CommunicationException {
+    public Iterator<Batch> search(String batchID, Integer roundTripNumber, List<String> pastSuccessfulEvents,
+                                  List<String> pastFailedEvents, List<String> futureEvents) throws
+                                                                                            CommunicationException {
 
         try {
-            SearchWS summaSearch = new SearchWSService(new java.net.URL(summaLocation),
-                                                       new QName("http://statsbiblioteket.dk/summa/search",
-                                                                 "SearchWSService")).getSearchWS();
             JSONObject jsonQuery = new JSONObject();
             jsonQuery.put("search.document.resultfields", PREMIS);
-            jsonQuery.put("search.document.query",
-                          toQueryString(batchID, roundTripNumber, pastSuccessfulEvents, pastFailedEvents, futureEvents));
+            jsonQuery.put(
+                    "search.document.query",
+                    toQueryString(batchID, roundTripNumber, pastSuccessfulEvents, pastFailedEvents, futureEvents));
             jsonQuery.put("search.document.startindex", 0);
             jsonQuery.put("search.document.maxrecords", 10);
 
@@ -81,14 +70,14 @@ public class SBOIClientImpl
             XPathSelector xPath = DOM.createXPathSelector();
 
 
-            NodeList nodeList =
-                    xPath.selectNodeList(searchResultDOM, "//responsecollection/response/documentresult/record");
+            NodeList nodeList = xPath.selectNodeList(
+                    searchResultDOM, "//responsecollection/response/documentresult/record");
 
             List<Batch> results = new ArrayList<>(nodeList.getLength());
             for (int i = 0; i < nodeList.getLength(); ++i) {
                 Node node = nodeList.item(i);
-                Batch batch = premisManipulatorFactory
-                        .createFromBlob(new ByteArrayInputStream(
+                Batch batch = premisManipulatorFactory.createFromBlob(
+                        new ByteArrayInputStream(
                                 DOM.selectString(node, "field[@name='" + PREMIS + "']").getBytes())).toBatch();
                 results.add(batch);
             }
@@ -101,23 +90,21 @@ public class SBOIClientImpl
 
     /**
      * Retrieve a batch from the summa index
-     * @param batchID the batch id
+     *
+     * @param batchID         the batch id
      * @param roundTripNumber the round trip number
+     *
      * @return the batch if found
      * @throws CommunicationException if the communication failed
-     * @throws NotFoundException if the described batch could not be found
+     * @throws NotFoundException      if the described batch could not be found
      */
     @Override
-    public Batch getBatch(String batchID,
-                          Integer roundTripNumber)
-            throws
-            CommunicationException,
-            NotFoundException {
+    public Batch getBatch(String batchID, Integer roundTripNumber) throws CommunicationException, NotFoundException {
         Iterator<Batch> result = search(batchID, roundTripNumber, null, null, null);
         while (result.hasNext()) {
             return result.next();
         }
-        throw new NotFoundException("batchid " + batchID.toString() + " not found");
+        throw new NotFoundException("batchid " + batchID + " not found");
     }
 
     private String spaced(String string) {
@@ -130,18 +117,17 @@ public class SBOIClientImpl
 
     /**
      * Format the retrictions as a summa query string
-     * @param batchID the batch id
-     * @param roundTripNumber the round trip number
+     *
+     * @param batchID              the batch id
+     * @param roundTripNumber      the round trip number
      * @param successfulPastEvents the successful events the batch must have experienced
-     * @param failedPastEvents the failed events the batch must have experienced
-     * @param futureEvents the events the batch must not have experienced
+     * @param failedPastEvents     the failed events the batch must have experienced
+     * @param futureEvents         the events the batch must not have experienced
+     *
      * @return the query string
      */
-    private String toQueryString(String batchID,
-                                 Integer roundTripNumber,
-                                 List<String> successfulPastEvents,
-                                 List<String> failedPastEvents,
-                                 List<String> futureEvents) {
+    private String toQueryString(String batchID, Integer roundTripNumber, List<String> successfulPastEvents,
+                                 List<String> failedPastEvents, List<String> futureEvents) {
 
         String base = spaced(RECORD_BASE);
         if (batchID != null) {
