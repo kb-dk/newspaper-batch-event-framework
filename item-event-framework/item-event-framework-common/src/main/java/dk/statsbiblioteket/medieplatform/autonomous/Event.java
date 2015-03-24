0d060624695fc474eb9a039830a1e2bd51653697
ktc <ktc@statsbiblioteket.dk>
2015-03-24 11:36:23 +0100

    add toString, equals and hashCode to Event class

--- item-event-framework/item-event-framework-common/src/main/java/dk/statsbiblioteket/medieplatform/autonomous/Event.java
+++ item-event-framework/item-event-framework-common/src/main/java/dk/statsbiblioteket/medieplatform/autonomous/Event.java
@@ -45,4 +45,45 @@ public class Event {
     public void setDate(Date date) {
         this.date = date;
     }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((date == null) ? 0 : date.hashCode());
+        result = prime * result + ((eventID == null) ? 0 : eventID.hashCode());
+        result = prime * result + (success ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Event other = (Event) obj;
+        if (date == null) {
+            if (other.date != null)
+                return false;
+        } else if (!date.equals(other.date))
+            return false;
+        if (eventID == null) {
+            if (other.eventID != null)
+                return false;
+        } else if (!eventID.equals(other.eventID))
+            return false;
+        if (success != other.success)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Event [eventID=" + eventID + ", success=" + success
+                + ", details=" + details + ", date=" + date + "]";
+    }
+
 }
